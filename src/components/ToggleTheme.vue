1578b453ff456c7641447fdc92434d96ffaeacdd
Franco Campaiola <francocampaiola@hotmail.com>
2023-06-15 19:35:23 -0300

    add Toggle to HomeView

---
+++ src/components/ToggleTheme.vue
@@ -0,0 +1,11 @@
+<template>
+    <v-switch v-model="$vuetify.theme.dark"
+         >
+    </v-switch>
+</template>
+
+<script>
+export default {
+    name: 'ToggleTheme',
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,27 @@
 <template>
     <v-container>
-        <p>Home</p>
+        <v-app-bar color="transparent" flat="false">
+            <v-spacer></v-spacer>
+            <v-row align="center">
+                <v-btn icon>
+                    <v-icon>mdi-heart</v-icon>
+                </v-btn>
+                <ToggleTheme />
+                <v-btn icon>
+                    <v-icon>mdi-heart</v-icon>
+                </v-btn>
+            </v-row>
+        </v-app-bar>
     </v-container>
 </template>
 
 <script>
+import ToggleTheme from '../components/ToggleTheme.vue';
 export default {
     name: 'HomeView',
+    components: {
+        ToggleTheme
+    },
     data() {
         return {
             //
