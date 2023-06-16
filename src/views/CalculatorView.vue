5bc9782e4579beb4128475fc6b9804eb2fbd1fce
Franco Campaiola <francocampaiola@hotmail.com>
2023-06-16 17:29:15 -0300

    add Calculator and History views

--- src/views/CalculatorView.vue
+++ src/views/CalculatorView.vue
@@ -1,10 +1,83 @@
 <template>
     <v-container>
-        <p>Calculator</p>
+        <v-app-bar color="transparent" flat>
+            <v-app-bar-title>Calculadora</v-app-bar-title>
+            <v-spacer></v-spacer>
+            <ToggleTheme />
+        </v-app-bar>
+        <v-container>
+            <v-card min-height="345" width="755" class="d-flex align-center mx-auto justify-center mb-6 rounded-lg mt-10">
+                <v-row>
+                    <v-col>
+                        <v-container>
+                            <div>
+                                <div class="text-body-1 caption mb-2">Cuenta
+                                </div>
+                                <v-text-field solo prepend-inner-icon="mdi-currency-usd" color="success"
+                                    type="number"></v-text-field>
+                            </div>
+                            <div>
+                                <div class="text-body-1 caption mb-2">Seleccione la propina</div>
+                                <v-col>
+                                    <v-row class="pa-2">
+                                        <v-btn x-large size="lg" class="me-2">5%</v-btn>
+                                        <v-btn x-large class="me-2">5%</v-btn>
+                                        <v-btn x-large>5%</v-btn>
+                                    </v-row>
+                                    <v-row class="pa-2">
+                                        <v-btn x-large class="me-2">5%</v-btn>
+                                        <v-btn x-large class="me-2">5%</v-btn>
+                                        <v-btn x-large>5%</v-btn>
+                                    </v-row>
+                                </v-col>
+                            </div>
+                            <div class="mt-5">
+                                <div class="text-body-1 caption mb-2">Personas</div>
+                                <v-text-field solo prepend-inner-icon="mdi-account" color="success"
+                                    type="number"></v-text-field>
+                            </div>
+                        </v-container>
+                    </v-col>
+                    <v-col>
+                        <v-container>
+                            <v-card color="teal accent-4" min-height="400">
+                                <v-container>
+                                    <div class="mb-16">
+                                        <v-row class="mt-5 align-center mx-auto">
+                                            <v-col>
+                                                <div class="text-body-1 caption mb-2">Propina</div>
+                                                <div class="text-body-2 caption mb-2">/ persona</div>
+                                            </v-col>
+                                            <v-col>
+                                                <div class="text-end text-h5 caption mb-2">$0</div>
+                                            </v-col>
+                                        </v-row>
+                                        <v-row class="align-center mx-auto">
+                                            <v-col>
+                                                <div class="text-body-1 caption mb-2">Propina</div>
+                                                <div class="text-body-2 caption mb-2">/ persona</div>
+                                            </v-col>
+                                            <v-col>
+                                                <div class="text-end text-h5 caption mb-2">$0</div>
+                                            </v-col>
+                                        </v-row>
+                                    </div>
+                                    <div>
+                                        <v-btn block class="mb-3">Calcular</v-btn>
+                                        <v-btn block>Limpiar</v-btn>
+                                    </div>
+                                </v-container>
+                            </v-card>
+                        </v-container>
+                    </v-col>
+                </v-row>
+            </v-card>
+        </v-container>
     </v-container>
 </template>
 
 <script>
+import ToggleTheme from '../components/ToggleTheme.vue';
 export default {
     name: 'CalculatorView',
     data() {
@@ -12,5 +85,8 @@ export default {
             //
         }
     },
+    components: {
+        ToggleTheme
+    }
 }
 </script>
--- src/views/HistoryView.vue
+++ src/views/HistoryView.vue
@@ -1,10 +1,33 @@
 <template>
     <v-container>
-        <p>History</p>
+        <v-app-bar color="transparent" flat>
+            <v-app-bar-title>Historial</v-app-bar-title>
+            <v-spacer></v-spacer>
+            <ToggleTheme />
+        </v-app-bar>
+        <v-row>
+            <v-col>
+                <v-card>
+                    <v-card-title>Propinas dadas</v-card-title>
+                    <v-card>
+                        <v-card-text>Propina 1</v-card-text>
+                    </v-card>
+                </v-card>
+            </v-col>
+            <v-col>
+                <v-card>
+                    <v-card-title>Cálculos realizados</v-card-title>
+                    <v-card>
+                        <v-card-text>Cálculo 1</v-card-text>
+                    </v-card>
+                </v-card>
+            </v-col>
+        </v-row>
     </v-container>
 </template>
 
 <script>
+import ToggleTheme from '../components/ToggleTheme.vue';
 export default {
     name: 'HistoryView',
     data() {
@@ -12,5 +35,8 @@ export default {
             //
         }
     },
+    components: {
+        ToggleTheme
+    }
 }
 </script>
