90a0eda51a6404dbc275e53bd4dd26d6f6d60213
Franco Campaiola <francocampaiola@hotmail.com>
2023-06-18 16:37:07 -0300

    add History Calculos

--- src/views/CalculatorView.vue
+++ src/views/CalculatorView.vue
@@ -13,14 +13,14 @@
                             <div>
                                 <div class="text-body-1 caption mb-2">Cuenta
                                 </div>
-                                <v-text-field solo prepend-inner-icon="mdi-currency-usd" color="success"
-                                    type="number"></v-text-field>
+                                <v-text-field solo prepend-inner-icon="mdi-currency-usd" color="success" type="number"
+                                    v-model="cuenta"></v-text-field>
                             </div>
                             <div>
                                 <div class="text-body-1 caption mb-2">Seleccione la propina</div>
                                 <v-col>
                                     <v-row class="pa-2">
-                                        <v-btn-toggle v-model="propina" required color="#00BFA5">
+                                        <v-btn-toggle v-model="porcentajePropina" required color="#00BFA5">
                                             <v-btn x-large size="lg" class="me-2" value="5">5%</v-btn>
                                             <v-btn x-large class="me-2" value="10">10%</v-btn>
                                             <v-btn x-large value="15">15%</v-btn>
@@ -30,8 +30,8 @@
                             </div>
                             <div class="mt-5">
                                 <div class="text-body-1 caption mb-2">Personas</div>
-                                <v-text-field solo prepend-inner-icon="mdi-account" color="success"
-                                    type="number"></v-text-field>
+                                <v-text-field solo prepend-inner-icon="mdi-account" color="success" type="number"
+                                    v-model="personas"></v-text-field>
                             </div>
                         </v-container>
                     </v-col>
@@ -46,22 +46,22 @@
                                                 <div class="text-body-2 caption mb-2">/ persona</div>
                                             </v-col>
                                             <v-col>
-                                                <div class="text-end text-h5 caption mb-2">$0</div>
+                                                <div class="text-end text-h5 caption mb-2">$ {{ propinaPorPersona }}</div>
                                             </v-col>
                                         </v-row>
                                         <v-row class="align-center mx-auto">
                                             <v-col>
-                                                <div class="text-body-1 caption mb-2">Propina</div>
+                                                <div class="text-body-1 caption mb-2">Total</div>
                                                 <div class="text-body-2 caption mb-2">/ persona</div>
                                             </v-col>
                                             <v-col>
-                                                <div class="text-end text-h5 caption mb-2">$0</div>
+                                                <div class="text-end text-h5 caption mb-2">$ {{ totalPorPersona }}</div>
                                             </v-col>
                                         </v-row>
                                     </div>
                                     <div>
-                                        <v-btn block class="mb-3">Calcular</v-btn>
-                                        <v-btn block>Limpiar</v-btn>
+                                        <v-btn block class="mb-3" @click="calcular">Calcular</v-btn>
+                                        <v-btn block @click="limpiar">Limpiar</v-btn>
                                     </div>
                                 </v-container>
                             </v-card>
@@ -75,15 +75,81 @@
 
 <script>
 import ToggleTheme from '../components/ToggleTheme.vue';
+import arrayService from '../utils/arrayService';
 export default {
     name: 'CalculatorView',
     data() {
         return {
-            propina: 10
+            cuenta: null,
+            porcentajePropina: null,
+            personas: null,
+            propinaPorPersona: 0,
+            totalPorPersona: 0,
+            arr: [],
+            errores: []
         }
+    },
+    computed: {
+        hayErrores: function () {
+            return this.errores.length;
+        },
     },
     components: {
         ToggleTheme
+    },
+    methods: {
+        guardarArray: function () {
+            let arr = this.arr;
+            arrayService.setArray(arr);
+        },
+        calcular: function () {
+            this.errores = [];
+
+            if (!this.cuenta || this.cuenta <= 0 || isNaN(this.cuenta)) {
+                this.errores.push('El total de la cuenta es requerido y debe ser válido.');
+            }
+
+            if (!this.porcentajePropina) {
+                this.errores.push('El porcentaje de propina es requerido.');
+            }
+
+            if (!this.personas || this.personas <= 0 || isNaN(this.personas)) {
+                this.errores.push('La cantidad de comensales es requerida y debe ser válida.');
+            }
+
+            if (this.errores.length === 0) {
+                let nuevoObj = {
+                    cuenta: this.cuenta,
+                    porcentajePropina: this.porcentajePropina,
+                    personas: this.personas,
+                    propinaPorPersona: (parseInt(this.cuenta) * parseInt(this.porcentajePropina)) / 100,
+                    totalPorPersona: (parseInt(this.cuenta) + parseInt(this.propinaPorPersona)) / parseInt(this.personas)
+                }
+
+                if (!localStorage.calculo) {
+                    this.arr = [];
+                } else {
+                    this.arr = JSON.parse(localStorage.getItem('calculo'));
+                }
+
+                this.arr.push(nuevoObj);
+                localStorage.setItem('calculo', JSON.stringify(this.arr));
+
+                this.guardarArray();
+
+                this.propinaPorPersona = nuevoObj.propinaPorPersona.toFixed(2);
+                this.totalPorPersona = nuevoObj.totalPorPersona.toFixed(2);
+            } else {
+                console.log(this.errores);
+            }
+        },
+        limpiar: function () {
+            this.cuenta = null;
+            this.porcentajePropina = null;
+            this.personas = null;
+            this.propinaPorPersona = 0;
+            this.totalPorPersona = 0;
+        }
     }
 }
 </script>
--- src/views/HistoryView.vue
+++ src/views/HistoryView.vue
@@ -25,10 +25,24 @@
             </v-col>
             <v-col>
                 <v-card>
-                    <v-card-title>Cálculos realizados</v-card-title>
-                    <v-card>
-                        <v-card-text>Cálculo 1</v-card-text>
-                    </v-card>
+                    <v-card-title>Calculos realizados</v-card-title>
+                    <v-col v-if="arr2.length === 0">
+                        <v-card-text>No hay cálculos</v-card-text>
+                    </v-col>
+                    <v-col cols="12" v-for="calculo in arr2" :key="calculo.id">
+                        <v-card>
+                            <v-card-title>Cuenta</v-card-title>
+                            <v-card-subtitle>Cuenta sin propina: $ {{ calculo.cuenta }}</v-card-subtitle>
+                            <v-card-text>Porcentaje de propina: {{ calculo.porcentajePropina }} %</v-card-text>
+                            <v-card-text>Cantidad de comensales: {{ calculo.personas }} persona(s)</v-card-text>
+                            <v-card-text>
+                                Propina por persona: $ {{ calculo.propinaPorPersona.toFixed(2) }}
+                            </v-card-text>
+                            <v-card-text>
+                                Total por persona: $ {{ calculo.totalPorPersona.toFixed(2) }}
+                            </v-card-text>
+                        </v-card>
+                    </v-col>
                 </v-card>
             </v-col>
         </v-row>
@@ -43,7 +57,8 @@ export default {
     name: 'HistoryView',
     data() {
         return {
-            arr: []
+            arr: [],
+            arr2: []
         };
     },
     components: {
@@ -52,13 +67,22 @@ export default {
     computed: {
         arrayPropinas() {
             return arrayService.getArray();
+        },
+        arrayCalculos() {
+            return arrayService.getArray();
         }
     },
     mounted: function () {
         const storedArray = localStorage.getItem('dato');
+        const storedArray2 = localStorage.getItem('calculo');
         if (storedArray) {
             this.arr = JSON.parse(storedArray);
         }
+
+        if (storedArray2) {
+            this.arr2 = JSON.parse(storedArray2);
+        }
+
     },
     methods: {
         // ...
